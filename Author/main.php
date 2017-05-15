0038c027a50ea852fcd1dfeee1339f29d8ee9264
Lazer <lazergamez290@gmail.com>
2017-05-15 10:53:34 +1000

    Update main.php

--- Author/main.php
+++ Author/main.php
@@ -1,1 +1,15 @@
-
+<?php
+namespace Author;
+use pocketmine\Server;
+use pocketmine\plugin\PluginBase;
+class main extends PluginBase {
+    public function onLoad(){
+        $this->getLogger()->info("{Plugin Name} loading.");
+    }
+    public function onEnable(){
+        $this->getLogger()->info("{Plugin Name} enabled.");
+    }
+    public function onDisable(){
+        $this->getLogger()->info("{Plugin Name] disabled.");
+    }
+}
